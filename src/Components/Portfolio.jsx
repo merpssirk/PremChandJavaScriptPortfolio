14f3af72b01206e8786c8cd259b739f0767cbc8b
Prem Bahadur Chand <61269298+merpssirk@users.noreply.github.com>
2023-07-24 09:49:14 +0200

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "STERLINK Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I have successfully created STERLINK GmbH website with a focus on user experience and functionality.",
+    url: "https://sterlink.de/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "pbc-webdev website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "I have successfully created STERLINK GmbH website with a focus on user experience and functionality.",
+    url: "https://pbc-webdev.com",
   },
   {
-    title: "My Resume Site",
+    title: "Many Canvas Power Apps and Power Automate",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "I have developed numerous canvas Power Apps that seamlessly integrate data and functionality, empowering businesses to streamline their processes and boost productivity.",
+    url: "https://learn.microsoft.com/en-us/users/prembahadurchand-6649/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub projects",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I have created many Projects in Github",
+    url: "https://github.com/merpssirk",
   },
 ];
 
